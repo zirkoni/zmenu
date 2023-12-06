ad8ab1a053709fd36ff66ea75c7c949685c74f5e
zirkoni <zirkoni88@hotmail.com>
2023-12-06 15:37:30 +0200

    Add search function
    - Press "S" to start search
    - Press "F3" to repeat previous search

--- INPUT.C
+++ INPUT.C
@@ -2,22 +2,55 @@
 #include "int.h"
 #include "position.h"
 #include <conio.h>
+#include <string.h>
 
-#define KEY_SELECT     13
-#define KEY_QUIT       27
-#define KEY_UP        328
-#define KEY_DOWN      336
-#define KEY_LEFT      331
-#define KEY_RIGHT     333
-#define KEY_BACKSPACE   8
+#define MAX_SEARCH_LENGTH 32
 
-void printInput(index_t* inputIndex)
+#define KEY_SELECT           13
+#define KEY_QUIT             27
+#define KEY_UP              328
+#define KEY_DOWN            336
+#define KEY_LEFT            331
+#define KEY_RIGHT           333
+#define KEY_BACKSPACE         8
+#define KEY_SEARCH          115 // S
+#define KEY_SEARCH_AGAIN    317 // F3
+
+int searchLength = 0;
+char searchString[MAX_SEARCH_LENGTH] = {0};
+
+void printSearchInput(const char* searchString)
 {
     screen(INT86_SET_CURSOR_HOME);
-    printf("Goto index: %lu          \n", *inputIndex); // Write spaces to overwrite deleted (backspace) digits
+    printf("Search: %s ", searchString);
 }
 
-void appendInputIndex(int key, index_t* inputIndex)
+void printSearchNotFound(const char* searchString)
+{
+    screen(INT86_SET_CURSOR_HOME);
+    printf("Unable to find \"%s\"", searchString);
+}
+
+void appendSearchString(int key, int* len, char* searchString)
+{
+    if(key >= ' ' && key <= '~' && *len < MAX_SEARCH_LENGTH)
+    {
+        searchString[*len] = key;
+        (*len)++;
+    } else if(key == KEY_BACKSPACE && *len > 0)
+    {
+        (*len)--;
+        searchString[*len] = '\0';
+    }
+}
+
+void printGotoInput(index_t* inputIndex)
+{
+    screen(INT86_SET_CURSOR_HOME);
+    printf("Goto index: %lu ", *inputIndex);
+}
+
+void appendGotoIndex(int key, index_t* inputIndex)
 {
     if(key >= '0' && key <= '9')
     {
@@ -33,11 +66,119 @@ void appendInputIndex(int key, index_t* inputIndex)
     }
 }
 
-bool handleAdditionalInput(int key, index_t* inputIndex)
+void updateSearchPosition(struct ProgramStatus* status, bool columns)
+{
+    if(!columns)
+    {
+        updatePositionDown(&status->selected, &status->first, &status->last, columns);
+    } else
+    {
+        if(status->selected % 2)
+        {
+            updatePositionLeft(&status->selected, &status->first, &status->last, columns);
+            updatePositionDown(&status->selected, &status->first, &status->last, columns);
+        } else
+        {
+            if(status->selected != g_numItems - 1)
+            {
+                updatePositionRight(&status->selected, &status->first, &status->last, columns);
+            } else
+            {
+                updatePositionDown(&status->selected, &status->first, &status->last, columns);
+            }
+        }
+    }
+}
+
+void doSearch(const char* searchString, struct ProgramStatus* status, struct SMenuItem* menu, bool columns, bool isNew)
+{
+    char* resultPtr = NULL;
+    index_t oldSelected = status->selected;
+    index_t oldFirst = status->first;
+    index_t oldLast = status->last;
+    bool found = false;
+
+    struct SMenuItem* item = menu;
+    index_t searchIdx = 0;
+    index_t searchEnd;
+
+    // Start from the next item if we are repeating search
+    if(!isNew)
+    {
+        updateSearchPosition(status, columns);
+    }
+
+    if(status->selected != 0)
+    {
+        searchEnd = status->selected - 1;
+    } else
+    {
+        searchEnd = g_numItems - 1;
+    }
+
+    while(searchIdx != status->selected)
+    {
+        ++searchIdx;
+        item = item->next;
+    }
+
+    while(true)
+    {
+        // NB: The index number at the beginning is included in the search => TODO: fix
+        resultPtr = strstr(item->name, searchString);
+
+        if(resultPtr != NULL)
+        {
+            found = true;
+            break;
+        }
+
+        if(searchIdx == searchEnd)
+        {
+            break;
+        }
+
+        updateSearchPosition(status, columns);
+        searchIdx = status->selected;
+
+        if(searchIdx != 0)
+        {
+            item = item->next;
+        } else
+        {
+            item = menu;
+        }
+    }
+
+    if(!found)
+    {
+        printSearchNotFound(searchString);
+        status->selected = oldSelected;
+        status->first = oldFirst;
+        status->last = oldLast;
+        while(!kbhit()) {}
+        checkKey(); // Remove pressed key from buffer
+    }
+}
+
+bool handleAdditionalInput(int key, struct ProgramStatus* status, struct SMenuItem* menu, bool columns)
 {
     bool changeToIndex = false;
-    appendInputIndex(key, inputIndex);
-    printInput(inputIndex);
+    bool search = false;
+
+    if(key == KEY_SEARCH)
+    {
+        search = true;
+        printSearchInput(searchString);
+    } else if(key == KEY_SEARCH_AGAIN && searchLength > 0)
+    {
+        doSearch(searchString, status, menu, columns, false);
+        return false;
+    } else
+    {
+        appendGotoIndex(key, &status->inputIndex);
+        printGotoInput(&status->inputIndex);
+    }
 
     while(true)
     {
@@ -48,14 +189,22 @@ bool handleAdditionalInput(int key, index_t* inputIndex)
             if(key == KEY_QUIT)
             {
                 break;
+            } else if(key == KEY_SELECT && search)
+            {
+                doSearch(searchString, status, menu, columns, true);
+                break;
             } else if(key == KEY_SELECT)
             {
                 changeToIndex = true;
                 break;
+            } else if(search)
+            {
+                appendSearchString(key, &searchLength, searchString);
+                printSearchInput(searchString);
             } else
             {
-                appendInputIndex(key, inputIndex);
-                printInput(inputIndex);
+                appendGotoIndex(key, &status->inputIndex);
+                printGotoInput(&status->inputIndex);
             }
         }
     }
@@ -92,7 +241,7 @@ bool handleInput(struct ProgramStatus* status, struct SMenuItem* menu, bool colu
                 status->quit = true;
                 break;
             default:
-                status->changeToIndex = handleAdditionalInput(key, &status->inputIndex);
+                status->changeToIndex = handleAdditionalInput(key, status, menu, columns);
                 break;
         }
 
